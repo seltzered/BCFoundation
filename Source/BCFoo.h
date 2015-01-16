1857102fab385cf184e42a6cba04ad90e0ee3df9
Pieter Omvlee <mail@pieteromvlee.net>
2015-01-16 10:17:51 +0100

    Trying to fix broken builds

--- Source/BCFoo.h
+++
@@ -1,8 +0,0 @@
-//  Created by Pieter Omvlee on 15/01/2015.
-//  Copyright (c) 2015 Bohemian Coding. All rights reserved.
-
-#import <Foundation/Foundation.h>
-
-@interface BCFoo : NSObject
-- (void)foo;
-@end
