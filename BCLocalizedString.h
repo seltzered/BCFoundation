a8fc3fb9c51030b98f842d6a18330241336321f4
Sam Deane <sam@elegantchaos.com>
2015-02-26 11:54:43 +0000

    Better comments and a slight tweak to the bundle stuff.

--- BCLocalizedString.h
+++ BCLocalizedString.h
@@ -3,4 +3,14 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+
+ A replacement for NSLocalizedString.
+ 
+ We don't pass a second description parameter, instead we rely on the key always being something descriptive (and therefore the actual value of the string always being looked up, even for the english version).
+ 
+ This is currently implemented very simply, but there's lots of scope for adding a debug implementation that can do clever things like adding "fake" translations, logging out details of missing keys, etc.
+ 
+ */
+
 extern NSString* BCLocalizedString(NSString* key);
--- BCLocalizedString.m
+++ BCLocalizedString.m
@@ -3,15 +3,30 @@
 
 #import "BCLocalizedString.h"
 
+/**
+ Currently this exists purely to set the bundle to use for translations.
+ 
+ We want to use the bundle containing this code for looking up translations, rather than just using the main bundle, so
+ that we can add localizations to a unit tests bundle and be able to pick them up from unit tests. 
+ 
+ When linked into an application, we will be in the main bundle so everything will work as expected.
+ */
+
 @interface BCLocalizedStringHelper : NSObject
 @end
 
 @implementation BCLocalizedStringHelper
++(NSBundle*)bundleForTranslations {
+  return [NSBundle bundleForClass:self];
+}
 @end
 
 NSString* BCLocalizedString(NSString* key) {
-  NSBundle* bundle = [NSBundle bundleForClass:[BCLocalizedStringHelper class]];
-  NSString* result = [bundle localizedStringForKey:key value:@"" table:nil];
+  static NSBundle* bundleForTranslations = nil;
+  if (!bundleForTranslations)
+    bundleForTranslations = [BCLocalizedStringHelper bundleForTranslations];
+  
+  NSString* result = [bundleForTranslations localizedStringForKey:key value:@"" table:nil];
   
   return result;
 }
